d09ffbc6930785694a07f83c6a2c9ae77c0c34f1
Prosper <hazardprosper227@gmail.com>
2026-01-16 02:15:57 +0100

    add props interfaces

---
+++ src/interfaces/input-row.interface.ts
@@ -0,0 +1,13 @@
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import { ComponentProps } from "react";
+
+// This extracts the 'name' prop type specifically
+type MaterialIconName = ComponentProps<typeof MaterialCommunityIcons>["name"];
+
+export interface InputRowProps {
+  className?: string;
+  prefixIcon?: MaterialIconName;
+  placeholder?: string;
+  isPassword?: boolean;
+  onChange?: (text: string) => void;
+}
