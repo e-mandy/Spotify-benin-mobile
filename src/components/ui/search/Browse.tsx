347d1ec9dd70da6d35203aa6c7f03d26966ec903
e-mandy <andymfrd02@gmail.com>
2026-02-09 16:36:14 +0100

    feat(search): Category component and browse section ui finished

--- src/components/ui/search/Browse.tsx
+++ src/components/ui/search/Browse.tsx
@@ -2,19 +2,28 @@ import { CATEOGORY_MOCK } from "@/mocks/categories.mock";
 import React from "react";
 import { FlatList, View } from "react-native";
 import { StyledText } from "../common";
-import Cateogory from "./Cateogory";
+import Category from "./Category";
 
 const Browse = () => {
   return (
     <View>
-      <View>
-        <StyledText className="font-bold">Parcourir tout</StyledText>
-      </View>
       <FlatList
         data={CATEOGORY_MOCK}
-        renderItem={({ item: genre }) => <Cateogory {...genre} />}
+        renderItem={({ item: genre }) => <Category {...genre} />}
         numColumns={2}
+        ListHeaderComponent={() => (
+          <View>
+            <StyledText className="font-bold">Parcourir tout</StyledText>
+          </View>
+        )}
+        ListHeaderComponentStyle={{
+          marginBottom: 15,
+        }}
         keyExtractor={(item) => item.id}
+        scrollEnabled={false}
+        columnWrapperStyle={{
+          justifyContent: "space-between",
+        }}
       />
     </View>
   );
---
+++ src/components/ui/search/Category.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Dimensions, ImageBackground, View } from "react-native";
+import { StyledText } from "../common";
+
+const { width } = Dimensions.get("screen");
+
+type CategoryProps = {
+  id: string;
+  name: string;
+  image: string;
+  color: string;
+};
+
+const Category = ({ name, image, color }: CategoryProps) => {
+  return (
+    <View
+      className="relative h-28 rounded-2xl overflow-hidden mb-4"
+      style={{ width: width * 0.43, backgroundColor: color }}
+    >
+      <StyledText className="text-xl font-bold absolute left-3 top-3">
+        {name}
+      </StyledText>
+      <View className="h-20 w-14 absolute bg-white bottom-[-5] right-[-5] rotate-[25deg]">
+        <ImageBackground
+          className="w-full h-full"
+          source={require("@/assets/images/mkay.jpeg")}
+          resizeMode="cover"
+        />
+      </View>
+    </View>
+  );
+};
+
+export default Category;
