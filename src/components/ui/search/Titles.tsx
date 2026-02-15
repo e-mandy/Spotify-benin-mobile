7e13cb11cf5e30a1bb639486cb936254b15b03a2
e-mandy <andymfrd02@gmail.com>
2026-02-15 15:21:57 +0100

    feat(search): Titles section based on search integrated with the backend successfully

--- src/components/ui/search/Titles.tsx
+++ src/components/ui/search/Titles.tsx
@@ -1,15 +1,18 @@
-import { MOCK_TITRES } from "@/mocks/titres.mocks";
+import { useSearchStore } from "@/src/store/search.store";
 import React from "react";
 import { View } from "react-native";
 import MusicTitle from "../common/MusicTitle";
 import SearchCollectionHeader from "../common/SearchCollectionHeader";
 
 const Titles = () => {
+  const searchTitles = useSearchStore((state) => state.searchResult?.titles);
+
+  if (!searchTitles) return null;
   return (
     <View>
       <SearchCollectionHeader title="Titres" path="/(tabs)/(search)/search" />
       <View className="w-full my-4">
-        {MOCK_TITRES.map((title, index) => (
+        {searchTitles.map((title, index) => (
           <MusicTitle key={index} {...title} />
         ))}
       </View>
