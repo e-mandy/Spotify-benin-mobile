d840ae6437c697da22a23995afcdb24d7f219589
e-mandy <andymfrd02@gmail.com>
2026-02-26 13:14:59 +0000

    feat(genre): Integration of the backend on the artist and titles section

--- src/components/ui/genre/ArtistsGenre.tsx
+++ src/components/ui/genre/ArtistsGenre.tsx
@@ -1,18 +1,27 @@
-import { artistesGenre } from "@/mocks/genres.mock";
+import { useGenreStore } from "@/src/store/genre.store";
+import { useLocalSearchParams } from "expo-router";
 import React from "react";
 import { View } from "react-native";
 import { FlatList } from "react-native-gesture-handler";
+import ResearchNotFound from "../common/ResearchNotFound";
 import SearchCollectionHeader from "../common/SearchCollectionHeader";
 import Artist from "./Artist";
 
 const ArtistsGenre = () => {
+  const genre = useLocalSearchParams() as { genre: string };
+  const artistsGenre = useGenreStore(
+    (state) => state.currentGenreInfo?.singers,
+  );
+
+  if (!artistsGenre || artistsGenre.length === 0)
+    return <ResearchNotFound section={`Artistes ${genre.genre}`} />;
   return (
     <View className="my-5">
       <SearchCollectionHeader title="Artistes Zinli" isAll={false} />
       <View className="mb-4">
         <FlatList
           className="py-5"
-          data={artistesGenre}
+          data={artistsGenre}
           renderItem={({ item: artist }) => <Artist {...artist} />}
           horizontal
           contentContainerStyle={{
