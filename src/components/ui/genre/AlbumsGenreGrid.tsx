54ca9d52262b67392f10ca54b50a3bd84687389a
e-mandy <andymfrd02@gmail.com>
2026-02-27 22:54:45 +0000

    feat(genre): Integration of the backend in the mobile ui

--- src/components/ui/genre/AlbumsGenreGrid.tsx
+++ src/components/ui/genre/AlbumsGenreGrid.tsx
@@ -1,13 +1,20 @@
-import { MOCK_ALBUMS } from "@/mocks/albums.mock";
+import { useGenreStore } from "@/src/store/genre.store";
+import { useLocalSearchParams } from "expo-router";
 import React from "react";
 import { FlatList } from "react-native-gesture-handler";
+import ResearchNotFound from "../common/ResearchNotFound";
 import AlbumGrid from "./AlbumGrid";
 
 const AlbumsGenreGrid = () => {
+  const genre = useLocalSearchParams() as { genre: string };
+  const albumsGenre = useGenreStore((state) => state.currentGenreInfo?.albums);
+
+  if (!albumsGenre || albumsGenre.length === 0)
+    return <ResearchNotFound section={`Albums - ${genre.genre}`} />;
   return (
     <FlatList
       className="py-5"
-      data={MOCK_ALBUMS}
+      data={albumsGenre}
       scrollEnabled={false}
       renderItem={({ item: album }) => <AlbumGrid {...album} />}
       contentContainerStyle={{
